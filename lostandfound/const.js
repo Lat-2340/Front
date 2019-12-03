3eb44c0f0d2f7dea0fd7be138b8df7e6083742c6
Xin Tong <xint5@illinois.edu>
2019-12-03 12:45:51 -0600

    Add const module & fetch lost items

---
+++ lostandfound/const.js
@@ -0,0 +1,3 @@
+export const IP_PORT = "http://127.0.0.1:8000/"
+export const LOST_AND_FOUND = "lostandfound/"
+export const GET_USER_LOST_ITEMS = "get-user-lost-items"
--- lostandfound/screen/myStatus/myStatus.js
+++ lostandfound/screen/myStatus/myStatus.js
@@ -16,122 +16,153 @@ import {
 } from "native-base";
 import {
   StyleSheet,
-  Image
+  Image,
+  Alert,
 } from 'react-native';
-// import styles from "./styles";
 
-// const sankhadeep = require("../../../assets/contacts/sankhadeep.png");
-// const supriya = require("../../../assets/contacts/supriya.png");
-// const himanshu = require("../../../assets/contacts/himanshu.png");
-// const shweta = require("../../../assets/contacts/shweta.png");
-// const shruti = require("../../../assets/contacts/shruti.png");
-// const shivraj = require("../../../assets/contacts/shivraj.png");
+import Login from "../login/login.js"
+
+import {
+  IP_PORT,
+  LOST_AND_FOUND,
+  GET_USER_LOST_ITEMS
+} from '../../const.js';
+
+const GET_LOST_ITEMS_URL = IP_PORT + LOST_AND_FOUND + GET_USER_LOST_ITEMS
+
 const test = require("./test.jpg");
+
+
 const datas = [
   {
     name: "item1",
     img: test,
-    items:[
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        }
+    items: [
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      }
     ]
   },
   {
     name: "item2",
     img: test,
-    items:[
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        }
+    items: [
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      }
     ]
   },
   {
     name: "item3",
     img: test,
-    items:[
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        }
+    items: [
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      }
     ]
   },
   {
     name: "item4",
     img: test,
-    items:[
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        },
-        {
-            img_match: test,
-            text: "Sankhadeep",
-            note: "Its time to build a difference . ."
-        }
+    items: [
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      },
+      {
+        img_match: test,
+        text: "Sankhadeep",
+        note: "Its time to build a difference . ."
+      }
     ]
   },
 ];
 
 class NHListThumbnail extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        showToast: false,
-        data: null
-      };
+    super(props);
+    this.state = {
+      showToast: false,
+      data: null,
+      lostItems: [],
+    };
 
   }
 
-  onPressDelete = async () =>{
+  onPressDelete = async () => {
 
+  }
+
+  fetchLostItems = async () => {
+    try {
+      const response = await fetch(GET_LOST_ITEMS_URL, {
+        method: 'GET',
+        headers: {
+          "Authorization": "Token " + Login.getToken()
+        },
+      })
+      const result = await response.json();
+      // console.log('Success:', result);
+
+      resLostItems = result['lost_items'].map(itemInfo => JSON.parse(itemInfo))
+      result['lost_images'].forEach((img, i) => {
+        resLostItems[i]['img'] = 'data:image/png;base64,' + img
+      });
+
+      this.setState({
+        lostItems: resLostItems
+      })
+
+    } catch (error) {
+      Alert.alert(error);
+      console.error(error);
+    }
   }
 
   componentWillMount() {
-      this.fetchLostItem();
-      console.log("show lost item");
-      console.log(this.state.username);
+    this.fetchLostItems();
   }
 
   render() {
@@ -149,54 +180,54 @@ class NHListThumbnail extends Component {
           <Right />
         </Header>
         <Content>
-        <List
-            dataArray={datas}
+          <List
+            dataArray={this.state.lostItems}
             renderRow={data =>
-            <List>
+              <List>
                 <ListItem itemHeader>
-                    <Image source={data.img} style={{width: '10%', height: '100%'}}  />
-                    <Text 
-                    style={{ 
-                        margin: 5,
-                        fontSize: 18,
-                        fontWeight: 'bold', 
-                        }} >
-                        {data.name}
-                    </Text>
+                  <Image source={{uri: data.img}} style={{ width: '10%', height: '100%' }} />
+                  <Text
+                    style={{
+                      margin: 5,
+                      fontSize: 18,
+                      fontWeight: 'bold',
+                    }} >
+                    {data.name}
+                  </Text>
                 </ListItem>
-                <List
-                    dataArray={data.items}
-                    renderRow={items =>
+                {/* <List
+                  dataArray={data.items}
+                  renderRow={items =>
                     <ListItem thumbnail>
-                        <Left>
+                      <Left>
                         <Thumbnail square source={items.img_match} />
-                        </Left>
-                        <Body>
+                      </Left>
+                      <Body>
                         <Text>
-                            {items.text}
+                          {items.text}
                         </Text>
                         <Text numberOfLines={1} note>
-                            {items.note}
+                          {items.note}
                         </Text>
-                        </Body>
-                        <Right>
-                        <Button transparent onPress={() => this.props.navigation.navigate("DetailPage",{items})}>
-                            <Text>Detail</Text>
+                      </Body>
+                      <Right>
+                        <Button transparent onPress={() => this.props.navigation.navigate("DetailPage", { items })}>
+                          <Text>Detail</Text>
                         </Button>
-                        </Right>
+                      </Right>
                     </ListItem>}
-                />
+                /> */}
                 <Button
-                    block
-                    danger
-                    style={{ margin:10 }}
-                    onPress={this.onPressDelete}
-                    >
-                    <Icon active name="trash" />
-                    <Text>Already found this item? Delete it now.</Text>
+                  block
+                  danger
+                  style={{ margin: 10 }}
+                  onPress={this.onPressDelete}
+                >
+                  <Icon active name="trash" />
+                  <Text>Already found this item? Delete it now.</Text>
                 </Button>
-            </List>}
-            />
+              </List>}
+          />
         </Content>
       </Container>
     );
